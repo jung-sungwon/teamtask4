50920e88a53a3abc6a88c1b3b65505a02c764fa5
chanloper <dbstjddl0396@naver.com>
2024-07-09 12:10:40 +0900

    home

--- app.py
+++ app.py
@@ -3,3 +3,14 @@ from flask import Flask, render_template, request
 import os
 
 app = Flask(__name__)
+
+# 초기 무승부, 승리, 패배 초기화 (기본)
+draw = 0
+win = 0
+lose = 0
+
+
+@app.route('/')
+def home():
+    # 초기화한 무승부/승/패 기록을 HTML에 렌더링하여 전달(우선)
+    return render_template('index.html')
